4c80edf508e76c5b2d5c6b932452201a1da526c1
Jee Vang, Ph.D <vangjee@gmail.com>
2020-06-09 00:55:13 -0400

    fix movement

--- python/pygame/key-movement.py
+++ python/pygame/key-movement.py
@@ -8,44 +8,40 @@ from pygame.locals import *
 class Image(object):
     def __init__(self, fpath, position, scale=1.0):
         img = pygame.image.load(fpath)
-        w, h = img.get_rect().size[0], img.get_rect().size[1]
+        w, h = img.get_rect(center=position).size[0], img.get_rect(center=position).size[1]
         w, h = int(w * scale), int(h * scale)
         img = pygame.transform.scale(img, (w, h))
 
+        x = position[0] - int(img.get_rect(center=position).size[0] / 2.0)
+        y = position[1] - int(img.get_rect(center=position).size[1] / 2.0)
+
         self.image = img
-        self.position = position
+        self.position = x, y
 
     def draw(self, surface):
         surface.blit(self.image, self.position)
 
     def update(self, width, height, event):
         x, y = self.position
-        dx, dy = 0, 0
+        rect = self.image.get_rect(center=self.position)
+        img_width = rect.size[0]
+        img_height = rect.size[1]
+
+        if event.key in (K_LEFT, K_a):
+            if x > 0:
+                x -= 10
+        elif event.key in (K_RIGHT, K_d):
+            if x + img_width < width:
+                x += 10
 
         if event.key in (K_UP, K_w):
-            dy = -10
-        if event.key in (K_DOWN, K_s):
-            dy = 10
-        if event.key in (K_LEFT, K_a):
-            dx = -10
-        if event.key in (K_RIGHT, K_d):
-            dx = 10
-
-        x += dx
-        y += dy
-
-        if x >= width:
-            x = width
-        elif x <= 0:
-            x = 0
-
-        if y >= height:
-            y = height
-        elif y <= 0:
-            y = 0
+            if y > 0:
+                y -= 10
+        elif event.key in (K_DOWN, K_s):
+            if y + img_height < height:
+                y += 10
 
         self.position = x, y
-        print(self.position)
 
     @staticmethod
     def get_rand_color(min_value=0, max_value=255):
@@ -81,7 +77,7 @@ def start():
     DISPLAYSURF.fill((255, 255, 255))
     pygame.display.set_caption('Key Events')
 
-    satyr = Image('./images/Satyr_01_Idle_000.png', (150, 150), 0.25)
+    satyr = Image('./images/Satyr_01_Idle_000.png', (200, 200), 0.25)
 
     while True:
         for event in pygame.event.get():
